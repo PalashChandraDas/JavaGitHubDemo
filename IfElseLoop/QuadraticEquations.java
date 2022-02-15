263e324e82e5d5bfbecfb6a3eeda3e52fb964175
Palash Chandra Das <85504305+PalashChandraDas@users.noreply.github.com>
2022-02-15 12:43:42 +0600

    Update QuadraticEquations.java

--- IfElseLoop/QuadraticEquations.java
+++ IfElseLoop/QuadraticEquations.java
@@ -9,16 +9,15 @@ public class QuadraticEquations {
 
         double D = b * b - (4 * a * c);
 
-        if (D < 0.0 || a == 0.0) {
-            System.out.println("Impossible");
-        } else if (D == 0.0) {
-            double x = -b / (2 * a);
-            System.out.printf("The root is real & equal:%.3f\n", x);
-        } else {// D>0
+        if (D > 0) {
             double x1 = -b + Math.sqrt(D) / (2 * a);
             double x2 = -b - Math.sqrt(D) / (2 * a);
             System.out.printf("Roots are real & unequal:%.3f, %.3f\n", x1, x2);
-
+        } else if (D == 0.0) {
+            double x = -b / (2 * a);
+            System.out.printf("The root is real & equal:%.3f\n", x);
+        } else {// D<0
+             System.out.println("Roots are imaginary");
         }
 
     }
