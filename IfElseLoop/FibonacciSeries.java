253854f96bcc28c323219509486594d1a0f2774f
Palash Chandra Das <85504305+PalashChandraDas@users.noreply.github.com>
2022-06-21 22:49:05 +0600

    Update FibonacciSeries.java

--- IfElseLoop/FibonacciSeries.java
+++ IfElseLoop/FibonacciSeries.java
@@ -1,22 +1,17 @@
-package basic_exercise;
-
-import java.util.Scanner;
-
-public class FibonacciSeries {
-
+class Fibonacci {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Enter nth series:");
-        int n = in.nextInt();
-        int a = 0;
-        int b = 1;
-        System.out.print("\n%d %d ", a, b);
 
-        for (int i = 3; i <= n; i++) {
-            int fibo = a + b;
-            a = b;
-            b = fibo;
-            System.out.print("%d ", fibo);
+        int firstTerm = 0, secTerm = 1, nextTerm, N = 15, i = 1;
+
+        while(i <= N){
+            System.out.print(firstTerm + " ");
+            
+            nextTerm = firstTerm + secTerm;
+            firstTerm = secTerm;
+            secTerm = nextTerm;
+            
+            i++;
         }
+
     }
 }
