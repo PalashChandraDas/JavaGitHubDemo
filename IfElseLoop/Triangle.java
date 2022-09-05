01aa0278ad4ea9c452c1e3f0ac6e7482ac9ebbc8
Palash Chandra Das <85504305+PalashChandraDas@users.noreply.github.com>
2022-09-05 22:18:59 +0600

    Update Triangle.java

--- IfElseLoop/Triangle.java
+++ IfElseLoop/Triangle.java
@@ -1,21 +1,18 @@
-package basic_exercise;
-
 import java.util.Scanner;
-
-public class Triangle {
-
+public class Exercise {
     public static void main(String[] args) {
+        System.out.print("Enter the the value of a, b & c: ");
         Scanner in = new Scanner(System.in);
-        System.out.print("The value of a, b, c = ");
-        double a = in.nextInt();
-        double b = in.nextInt();
-        double c = in.nextInt();
+        double a = in.nextDouble();
+        double b = in.nextDouble();
+        double c = in.nextDouble();
 
-        if (a != 0 && b != 0 && c != 0 && (a + b) > c && (b + c) > a && (c + a) > b) {
-            System.out.println("This is Triangle.");
+        if ((a + b) > c && (b + c) > a && (c + a) > b) {
+            double S = (a + b + c) / 2.0;
+            double Area = Math.sqrt(S * (S - a) * (S - b) * (S - c));
+            System.out.printf("The area of triangle is: %.3f\n", Area);
         } else {
-            System.out.println("Oh! Not Triangle.");
+            System.out.println("Triangle is not possible.");
         }
-
     }
 }
